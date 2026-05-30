e3e953b840b4919f700d6eccdf566f79f2de19f8
BackendPBO <findltadministrator@gmail.com>
2026-05-30 21:08:58 +0700

    Update AdminController.java

--- backend/Han Main/webtest/src/main/java/org/han/webtest/controller/AdminController.java
+++ backend/Han Main/webtest/src/main/java/org/han/webtest/controller/AdminController.java
@@ -19,7 +19,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class AdminController {
 
     private final AdminService adminService;

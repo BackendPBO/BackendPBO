d524922ca8c03a5a3ff662184fa0ad8be97b2ecf
BackendPBO <findltadministrator@gmail.com>
2026-05-30 21:09:34 +0700

    Update TrainerController.java

--- backend/Han Main/webtest/src/main/java/org/han/webtest/controller/TrainerController.java
+++ backend/Han Main/webtest/src/main/java/org/han/webtest/controller/TrainerController.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/trainer")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+
 public class TrainerController {
 
     private final ClassScheduleRepository classScheduleRepository;
